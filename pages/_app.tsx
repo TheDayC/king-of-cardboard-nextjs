cf3abf2c1d721c6d77a2855c0a4cc30b0b66320e
TheDayC <daveconnolly90@gmail.com>
2021-09-17 14:47:11 +0100

    Added redux state persistor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from '../store';
+import storeInstance from '../store';
 import '../styles/globals.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+    const { store, persistor } = storeInstance();
+
     return (
         <Provider store={store}>
-            <Component {...pageProps} />
+            <PersistGate loading={null} persistor={persistor}>
+                <Component {...pageProps} />
+            </PersistGate>
         </Provider>
     );
 };
--- store/index.ts
+++ store/index.ts
@@ -1,11 +1,26 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './slices';
 import { createInitialState } from './state';
 
-// Create our store and allow thunks for future development if required.
-const store = createStore(rootReducer, createInitialState(), composeWithDevTools(applyMiddleware(thunk)));
+const persistConfig = {
+    key: 'root',
+    storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+    // Create our store and allow thunks for future development if required.
+    const store = createStore(persistedReducer, createInitialState(), composeWithDevTools(applyMiddleware(thunk)));
+    const persistor = persistStore(store);
+
+    return {
+        store,
+        persistor,
+    };
+};
