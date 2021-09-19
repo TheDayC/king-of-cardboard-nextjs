4562999029449604c0772d9831380d2739bae57c
TheDayC <daveconnolly90@gmail.com>
2021-09-19 12:48:10 +0100

    Cleaned up cart page

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,31 +1,9 @@
 import React from 'react';
-import { GetStaticProps } from 'next';
 
 import Header from '../../components/Header';
 import Cart from '../../components/Cart';
-import { getCommerceAuth, initCommerceClient } from '../../utils/commerce';
-import { CartStaticProps } from '../../types/cart';
 
-export const getStaticProps: GetStaticProps = async () => {
-    const token = await getCommerceAuth();
-    const cl = token ? initCommerceClient(token.accessToken) : null;
-    const order = cl ? await cl.orders.create({ guest: true }) : null;
-
-    if (order) {
-        return {
-            props: {
-                order,
-            },
-        };
-    } else {
-        return {
-            props: {}, // will be passed to the page component as props
-        };
-    }
-};
-
-export const CartPage: React.FC<CartStaticProps> = ({ order }) => {
-    console.log('🚀 ~ file: index.tsx ~ line 28 ~ order', order);
+export const CartPage: React.FC = () => {
     return (
         <React.Fragment>
             <Header />
