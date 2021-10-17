c59fe8ec6ca50220599a28b2c41d37e2bfd75ed0
TheDayC <daveconnolly90@gmail.com>
2021-10-17 09:35:53 +0100

    Changed container to mui 5 in cart

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container } from '@mui/material';
 
 import Header from '../../components/Header';
 import Cart from '../../components/Cart';
@@ -7,9 +8,9 @@ export const CartPage: React.FC = () => {
     return (
         <React.Fragment>
             <Header />
-            <div className="container mx-auto p-8">
+            <Container maxWidth="xl">
                 <Cart />
-            </div>
+            </Container>
         </React.Fragment>
     );
 };
