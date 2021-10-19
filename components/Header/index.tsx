29db6b5009e32f77a519c411b756cff49b6e9548
TheDayC <daveconnolly90@gmail.com>
2021-10-19 11:33:29 +0100

    Added new logo, not quite right but better than last one.

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,24 +6,21 @@ import Link from 'next/link';
 
 import selector from './selector';
 import styles from './header.module.css';
-import logo from '../../images/logo1x.png';
+import logo from '../../images/logo-full.png';
 
 export const Header: React.FC = () => {
     const { cartItemCount } = useSelector(selector);
 
     return (
         <div className="navbar mb-4 shadow-md bg-neutral text-neutral-content">
-            <div className="flex-none px-2 mx-2">
+            <div className="navbar-start">
                 <div className={styles.logoWrapper}>
                     <Link href="/" passHref>
-                        <React.Fragment>
-                            <Image src={logo} alt="King of Cardboard Logo" title="King of Cardboard" />
-                            <span className="text-lg font-bold">King of Cardboard</span>
-                        </React.Fragment>
+                        <Image src={logo} alt="King of Cardboard Logo" title="King of Cardboard" />
                     </Link>
                 </div>
             </div>
-            <div className="flex-grow px-2 mx-2">
+            <div className="navbar-center">
                 <div className="items-stretch hidden lg:flex">
                     <Link href="/" passHref>
                         <button className="btn btn-ghost btn-sm rounded-btn">Home</button>
@@ -45,7 +42,7 @@ export const Header: React.FC = () => {
                     </Link>
                 </div>
             </div>
-            <div className="flex-none px-2 mx-2">
+            <div className="navbar-end">
                 <Link href="/cart" passHref>
                     <div className="flex justify-start items-center indicator cursor-pointer rounded-md hover:bg-neutral-focus">
                         <AiOutlineShoppingCart className={styles.cart} />
