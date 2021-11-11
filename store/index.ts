337a1cfac8336aec28167ff494b0415dc5c42228
TheDayC <daveconnolly90@gmail.com>
2021-11-11 20:28:59 +0000

    Remove redux debug and only show devTools in production

--- store/index.ts
+++ store/index.ts
@@ -18,7 +18,7 @@ const makeConfiguredStore = (rootReducer: any) =>
     configureStore({
         reducer: rootReducer,
         middleware: [thunkMiddleware],
-        devTools: true,
+        devTools: process.env.NODE_ENV !== 'production',
     });
 
 const makeStore = () => {
@@ -42,4 +42,4 @@ export type AppStore = ReturnType<typeof makeStore>;
 export type AppState = ReturnType<AppStore['getState']>;
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, Action>;
 
-export const wrapper = createWrapper<AppStore>(makeStore, { debug: true });
+export const wrapper = createWrapper<AppStore>(makeStore);
