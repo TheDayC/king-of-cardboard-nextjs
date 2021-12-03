f2eed9816c0a1fa54b3b521331c7eb90b49ca6d9
TheDayC <daveconnolly90@gmail.com>
2021-12-03 09:03:02 +0000

    Styled edit and delete buttons

--- components/Account/AddressBook/Address/index.tsx
+++ components/Account/AddressBook/Address/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import { BiTrash, BiEdit } from 'react-icons/bi';
 
 interface AddressProps {
     id: string;
@@ -8,17 +9,17 @@ interface AddressProps {
 
 export const Address: React.FC<AddressProps> = ({ id, name }) => {
     return (
-        <div className="flex flex-col cursor-pointer p-6 border border-solid border-base-200 justify-center items-center w-full rounded-md shadow-md">
-            <p>{name}</p>
-            <div className="flex flex-row w-full">
+        <div className="flex flex-col cursor-pointer p-6 border border-solid border-base-200 justify-center items-start w-full rounded-md shadow-md">
+            <p className="text-sm mb-4">{name}</p>
+            <div className="flex flex-row w-full justify-between">
                 <Link href={`/account/editAddress/${id}`} passHref>
-                    <button type="submit" className="btn">
-                        Edit
+                    <button type="submit" className="btn btn-md rounded-md">
+                        <BiEdit className="inline-block text-xl" />
                     </button>
                 </Link>
                 <Link href={`/account/deleteAddress/${id}`} passHref>
-                    <button type="submit" className="btn">
-                        Delete
+                    <button type="submit" className="btn btn-md btn-error rounded-md text-white">
+                        <BiTrash className="inline-block text-xl" />
                     </button>
                 </Link>
             </div>
