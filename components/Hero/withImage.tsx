46c25d227fb2e4c751a31aa991527ff9d64b32e6
TheDayC <daveconnolly90@gmail.com>
2021-12-19 13:40:22 +0000

    Updated home page mobile styling

--- components/Hero/withImage.tsx
+++ components/Hero/withImage.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import styles from './hero.module.css';
-
 interface HeroWithImageProps {
     title: string;
     content: string[];
@@ -22,15 +20,15 @@ export const HeroWithImage: React.FC<HeroWithImageProps> = ({
     shouldReverse,
 }) => {
     return (
-        <div className="hero p-6 bg-white">
-            <div className={`flex-col hero-content p-0 lg:flex-row${shouldReverse ? '-reverse' : ''}`}>
+        <div className="hero bg-white p-4 lg:p-6">
+            <div className={`flex flex-col hero-content p-0 lg:flex-row${shouldReverse ? '-reverse' : ''}`}>
                 {image_url && (
-                    <div className={styles.imageContainer}>
+                    <div className="rounded-md shadow-2xl relative overflow-hidden w-full h-40 hidden lg:inline-block lg:w-1/4 lg:h-80">
                         <Image src={image_url} alt={`${title}-hero-image`} layout="fill" objectFit="cover" />
                     </div>
                 )}
-                <div className={`${shouldReverse ? 'mr-4' : 'ml-4'}`}>
-                    <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+                <div className={`w-full lg:w-3/4 ${shouldReverse ? 'lg:mr-4' : 'lg:ml-4'}`}>
+                    <h1 className="font-bold mb-4 text-2xl lg:text-5xl">{title}</h1>
                     {content.length > 0 &&
                         content.map((contentItem, index) => (
                             <p className="mb-5" key={`hero-p-${index}`}>
@@ -39,7 +37,7 @@ export const HeroWithImage: React.FC<HeroWithImageProps> = ({
                         ))}
                     {link && (
                         <Link href={link} passHref>
-                            <button className="btn btn-primary">{link_title}</button>
+                            <button className="btn btn-primary btn-block rounded-md lg:btn-md">{link_title}</button>
                         </Link>
                     )}
                 </div>
