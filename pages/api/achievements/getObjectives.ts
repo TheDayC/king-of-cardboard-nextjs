be6f8dcf89248b0ac6fb0abb2bff87fb1c0ece0c
TheDayC <daveconnolly90@gmail.com>
2021-12-28 15:05:44 +0000

    getObjectives endpoint tidied

--- pages/api/achievements/getObjectives.ts
+++ pages/api/achievements/getObjectives.ts
@@ -1,11 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { connectToDatabase } from '../../../middleware/database';
-import { parseAsArrayOfStrings, parseAsNumber, parseAsString, safelyParse } from '../../../utils/parsers';
+import { errorHandler } from '../../../middleware/errors';
+import { parseAsArrayOfStrings, parseAsNumber, safelyParse } from '../../../utils/parsers';
+
+const defaultErr = 'We could not fetch your objectives.';
+
+function buildQuery(categories: string[] | null, types: string[] | null): object {
+    if (categories && types) {
+        return { $or: [{ category: { $in: categories } }, { types: { $in: types } }] };
+    } else if (categories) {
+        return { category: { $in: categories } };
+    } else if (types) {
+        return { types: { $in: types } };
+    } else {
+        return {};
+    }
+}
 
 async function getObjectives(req: NextApiRequest, res: NextApiResponse): Promise<void> {
     if (req.method === 'POST') {
-        const { db, client } = await connectToDatabase();
+        const { db } = await connectToDatabase();
 
         try {
             const categories = safelyParse(req, 'body.categories', parseAsArrayOfStrings, null);
@@ -17,31 +32,20 @@ async function getObjectives(req: NextApiRequest, res: NextApiResponse): Promise
             const objectivesCollection = db.collection('objectives');
 
             // Define our query based on what is passed. Leave empty by default.
-            let query = {};
-            if (categories && types) {
-                query = { $or: [{ category: { $in: categories } }, { types: { $in: types } }] };
-            } else if (categories) {
-                query = { category: { $in: categories } };
-            } else if (types) {
-                query = { types: { $in: types } };
-            }
+            const query = buildQuery(categories, types);
 
             const objectivesCount = await objectivesCollection.find(query).count();
             const objectivesDocument = await objectivesCollection.find(query).skip(skip).limit(limit).toArray();
+            const status = objectivesDocument ? 200 : 400;
+            const body = objectivesDocument
+                ? { objectives: objectivesDocument, count: objectivesCount }
+                : { status, message: 'Bad Request', description: defaultErr };
 
-            if (objectivesDocument) {
-                res.status(200).json({ objectives: objectivesDocument, count: objectivesCount });
-            } else {
-                res.status(404).json({ objectives: [] });
-            }
+            res.status(status).json(body);
         } catch (err: unknown) {
             const status = safelyParse(err, 'response.status', parseAsNumber, 500);
-            const statusText = safelyParse(err, 'response.statusText', parseAsString, '');
-            const message = safelyParse(err, 'response.data.errors', parseAsArrayOfStrings, [
-                'Something went very wrong! Likely a problem connecting to commercelayer.',
-            ]);
 
-            res.status(status).json({ status, statusText, message });
+            res.status(status).json(errorHandler(err, defaultErr));
         }
 
         return Promise.resolve();
