2abba5c3ce03ec9fc0668d8b49cb7aadc315f75d
TheDayC <daveconnolly90@gmail.com>
2021-12-28 15:13:06 +0000

    Tidied getAchievements endpoint

--- pages/api/achievements/getAchievements.ts
+++ pages/api/achievements/getAchievements.ts
@@ -1,19 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { connectToDatabase } from '../../../middleware/database';
-import {
-    parseAsArrayOfAchievements,
-    parseAsArrayOfStrings,
-    parseAsNumber,
-    parseAsString,
-    safelyParse,
-} from '../../../utils/parsers';
+import { errorHandler } from '../../../middleware/errors';
+import { parseAsArrayOfAchievements, parseAsNumber, parseAsString, safelyParse } from '../../../utils/parsers';
+
+const defaultErr = 'We could not fetch your achievements.';
 
 async function getAchievements(req: NextApiRequest, res: NextApiResponse): Promise<void> {
     if (req.method === 'POST') {
-        const { db, client } = await connectToDatabase();
-
         try {
+            const { db } = await connectToDatabase();
             const emailAddress = safelyParse(req, 'body.emailAddress', parseAsString, null);
 
             const achievementsCollection = db.collection('achievements');
@@ -30,16 +26,12 @@ async function getAchievements(req: NextApiRequest, res: NextApiResponse): Promi
 
                 res.status(200).json({ giftCardId, achievements });
             } else {
-                res.status(404).json({ achievements: [] });
+                res.status(400).json({ status: 400, message: 'Bad Request', description: defaultErr });
             }
-        } catch (err) {
+        } catch (err: unknown) {
             const status = safelyParse(err, 'response.status', parseAsNumber, 500);
-            const statusText = safelyParse(err, 'response.statusText', parseAsString, '');
-            const message = safelyParse(err, 'response.data.errors', parseAsArrayOfStrings, [
-                'Something went very wrong! Likely a problem connecting to commercelayer.',
-            ]);
 
-            res.status(status).json({ status, statusText, message });
+            res.status(status).json(errorHandler(err, defaultErr));
         }
 
         return Promise.resolve();
