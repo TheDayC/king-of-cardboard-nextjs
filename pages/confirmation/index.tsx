19ad2ea9bd6b55617150ff4f4a8713e5f022ab26
TheDayC <daveconnolly90@gmail.com>
2022-01-02 17:43:40 +0000

    Tidied generate order on the confirmation page.

--- pages/confirmation/index.tsx
+++ pages/confirmation/index.tsx
@@ -1,43 +1,41 @@
 import React, { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useSession } from 'next-auth/react';
 
 import Header from '../../components/Header';
 import { CommerceAuthProps } from '../../types/commerce';
 import Summary from '../../components/Checkout/Summary';
 import ConfirmationDetails from '../../components/ConfirmationDetails';
 import { setCheckoutLoading } from '../../store/slices/global';
-import { resetCart, setOrder } from '../../store/slices/cart';
+import { createCLOrder, resetCart } from '../../store/slices/cart';
 import { resetCheckoutDetails } from '../../store/slices/checkout';
 import selector from './selector';
-import { createOrder } from '../../utils/commerce';
 
 export const ConfirmationPage: React.FC<CommerceAuthProps> = () => {
     const { confirmationOrder, accessToken } = useSelector(selector);
     const dispatch = useDispatch();
+    const { data: session } = useSession();
+    const isGuest = !Boolean(session);
 
     // Create a brand new order and set the id in the store.
     const generateOrder = useCallback(
         async (accessToken: string) => {
-            const order = await createOrder(accessToken);
-
-            if (order) {
-                dispatch(setOrder(order));
-            }
+            dispatch(createCLOrder({ accessToken, isGuest }));
         },
-        [dispatch]
+        [dispatch, isGuest]
     );
 
     useEffect(() => {
         // Check to see if we've just arrived here from a successful order.
         if (confirmationOrder && accessToken) {
-            // Tell the system to generate a new order
-            generateOrder(accessToken);
-
             // Reset the cart state.
             dispatch(resetCart());
 
             // Reset the checkout data
             dispatch(resetCheckoutDetails());
+
+            // Tell the system to generate a new order
+            generateOrder(accessToken);
 
             // Checkout has finished loading by moving to the confirmation.
             dispatch(setCheckoutLoading(false));
