9a89fdc122599a49f4f29d0c3354a1cc7ffcdaf3
TheDayC <daveconnolly90@gmail.com>
2022-01-03 15:00:18 +0000

    Fixed order confirmation page

--- components/Checkout/Summary/Totals/selector.ts
+++ components/Checkout/Summary/Totals/selector.ts
@@ -1,11 +1,14 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-import { selectCartData } from '../../../../store/state/selectors';
+import { selectCartData, selectConfirmationData } from '../../../../store/state/selectors';
 
-const selector = createSelector([selectCartData], (cart) => ({
-    subTotal: cart.subTotal,
-    shipping: cart.shipping,
-    total: cart.total,
+const selector = createSelector([selectCartData, selectConfirmationData], (cart, confirmation) => ({
+    cartSubTotal: cart.subTotal,
+    cartShipping: cart.shipping,
+    cartTotal: cart.total,
+    confirmationSubTotal: confirmation.subTotal,
+    confirmationShipping: confirmation.shipping,
+    confirmationTotal: confirmation.total,
 }));
 
 export default selector;
