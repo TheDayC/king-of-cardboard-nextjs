4864801c58a5a0a318da49d51b4b35373e663358
TheDayC <daveconnolly90@gmail.com>
2022-01-09 17:56:21 +0000

    Removed home selector

--- pages/selector.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-
-import { selectPageData } from '../store/state/selectors';
-
-const selector = createSelector([selectPageData], (pageData) => ({
-    page: pageData.pages.find((page) => page.title.toLowerCase() === 'home') || null,
-}));
-
-export default selector;
