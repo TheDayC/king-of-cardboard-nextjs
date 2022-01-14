9ceaf91cd93a93e79904ba728e4f98966f0258e9
TheDayC <daveconnolly90@gmail.com>
2022-01-14 13:12:56 +0000

    Fixed layout

--- components/Hero/withImage.tsx
+++ components/Hero/withImage.tsx
@@ -21,28 +21,64 @@ export const HeroWithImage: React.FC<HeroWithImageProps> = ({
 }) => {
     return (
         <div className="hero bg-white">
-            <div className={`flex flex-col hero-content p-0 lg:flex-row lg:flex-row${shouldReverse ? '-reverse' : ''}`}>
-                {image_url && (
-                    <div className="rounded-md shadow-2xl relative overflow-hidden w-full h-40 hidden lg:inline-block lg:w-1/4 lg:h-80">
-                        <Image src={image_url} alt={`${title}-hero-image`} layout="fill" objectFit="cover" role="img" />
-                    </div>
+            <div className="flex flex-col hero-content p-0 lg:flex-row">
+                {shouldReverse ? (
+                    <React.Fragment>
+                        <div className="rounded-md shadow-2xl relative overflow-hidden w-full h-40 hidden lg:inline-block lg:w-1/4 lg:h-80">
+                            <Image
+                                src={image_url || ''}
+                                alt={`${title}-hero-image`}
+                                layout="fill"
+                                objectFit="cover"
+                                role="img"
+                            />
+                        </div>
+                        <div className="w-full lg:w-3/4 lg:ml-4">
+                            <h1 className="font-bold mb-4 text-2xl lg:text-5xl" role="heading">
+                                {title}
+                            </h1>
+                            {content.length > 0 &&
+                                content.map((contentItem, index) => (
+                                    <p className="mb-5" key={`hero-p-${index}`}>
+                                        {contentItem}
+                                    </p>
+                                ))}
+                            {link && (
+                                <Link href={link} passHref>
+                                    <button className="btn btn-primary w-full rounded-md lg:w-1/4">{link_title}</button>
+                                </Link>
+                            )}
+                        </div>
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                        <div className="w-full lg:w-3/4 lg:mr-4">
+                            <h1 className="font-bold mb-4 text-2xl lg:text-5xl" role="heading">
+                                {title}
+                            </h1>
+                            {content.length > 0 &&
+                                content.map((contentItem, index) => (
+                                    <p className="mb-5" key={`hero-p-${index}`}>
+                                        {contentItem}
+                                    </p>
+                                ))}
+                            {link && (
+                                <Link href={link} passHref>
+                                    <button className="btn btn-primary w-full rounded-md lg:w-1/4">{link_title}</button>
+                                </Link>
+                            )}
+                        </div>
+                        <div className="rounded-md shadow-2xl relative overflow-hidden w-full h-40 hidden lg:inline-block lg:w-1/4 lg:h-80">
+                            <Image
+                                src={image_url || ''}
+                                alt={`${title}-hero-image`}
+                                layout="fill"
+                                objectFit="cover"
+                                role="img"
+                            />
+                        </div>
+                    </React.Fragment>
                 )}
-                <div className={`w-full lg:w-3/4 ${shouldReverse ? 'lg:mr-4' : 'lg:ml-4'}`}>
-                    <h1 className="font-bold mb-4 text-2xl lg:text-5xl" role="heading">
-                        {title}
-                    </h1>
-                    {content.length > 0 &&
-                        content.map((contentItem, index) => (
-                            <p className="mb-5" key={`hero-p-${index}`}>
-                                {contentItem}
-                            </p>
-                        ))}
-                    {link && (
-                        <Link href={link} passHref>
-                            <button className="btn btn-primary w-full rounded-md lg:w-1/4">{link_title}</button>
-                        </Link>
-                    )}
-                </div>
             </div>
         </div>
     );
