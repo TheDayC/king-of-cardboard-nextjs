11be6a018d2f47c5a90805af0a6ae9a3b9dd2e59
TheDayC <daveconnolly90@gmail.com>
2022-01-14 14:35:07 +0000

    Fixed robotTxt

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,9 +1,11 @@
 module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://example.com',
     generateRobotsTxt: true,
-    robotsTxtOptions: [{ userAgent: '*', disallow: '/' }],
+    robotsTxtOptions: {
+        policies: [{ userAgent: '*', disallow: '/' }],
+    },
     exclude: [
-        '/account',
+        '/account/*',
         '/account/*',
         '/checkout',
         '/checkout/*',
