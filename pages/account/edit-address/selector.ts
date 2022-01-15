bb80aaace9d69fa2a2d781dec39a903ffba007e8
TheDayC <daveconnolly90@gmail.com>
2022-01-15 08:15:05 +0000

    edit address working as expected

--- pages/account/edit-address/selector.ts
+++ pages/account/edit-address/selector.ts
@@ -3,7 +3,7 @@ import { createSelector } from '@reduxjs/toolkit';
 import { selectGlobalData, selectAccountData } from '../../../store/state/selectors';
 
 const selector = createSelector([selectGlobalData, selectAccountData], (global, account) => ({
-    accessToken: global.accessToken,
+    accessToken: global.userToken || global.accessToken,
     currentAddress: account.currentAddress,
 }));
 
