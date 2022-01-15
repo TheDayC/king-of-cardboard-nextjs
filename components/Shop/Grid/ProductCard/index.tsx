418b5b8e2a53dde7f2898a041289fddd42b8c08b
TheDayC <daveconnolly90@gmail.com>
2022-01-15 19:26:01 +0000

    Added links to product page

--- components/Shop/Grid/ProductCard/index.tsx
+++ components/Shop/Grid/ProductCard/index.tsx
@@ -24,24 +24,35 @@ export const ProductCard: React.FC<CardProps> = ({
     slug,
 }) => {
     const shouldShowCompare = amount !== compareAmount && compareAmount.length > 0;
+    const linkOptions = {
+        pathname: '/product/[slug]',
+        query: { slug },
+    };
 
     return (
-        <div className="card shadow-md rounded-md bordered pt-4" data-testid="product-card">
+        <div
+            className="card shadow-md rounded-md bordered pt-4 transition duration-300 ease-in-out hover:shadow-2xl"
+            data-testid="product-card"
+        >
             {image.length > 0 && (
-                <div className="relative w-full h-40">
-                    <Image
-                        src={image}
-                        alt={imgDesc}
-                        title={imgTitle}
-                        layout="fill"
-                        objectFit="scale-down"
-                        className="rounded-sm"
-                    />
-                </div>
+                <Link href={linkOptions} passHref>
+                    <div className="relative w-full h-40 cursor-pointer">
+                        <Image
+                            src={image}
+                            alt={imgDesc}
+                            title={imgTitle}
+                            layout="fill"
+                            objectFit="scale-down"
+                            className="rounded-sm"
+                        />
+                    </div>
+                </Link>
             )}
             <div className="justify-between items-center card-body px-6 py-4">
                 <div className="flex flex-col justify-start items-center">
-                    <h2 className="card-title text-center text-2xl">{name}</h2>
+                    <Link href={linkOptions} passHref>
+                        <h2 className="card-title text-center text-2xl cursor-pointer hover:underline">{name}</h2>
+                    </Link>
                     {tags.length > 0 && (
                         <div className="flex flex-row flex-wrap justify-start items-center">
                             {tags.map((tag) => (
@@ -59,13 +70,7 @@ export const ProductCard: React.FC<CardProps> = ({
                         )}
                         <span className="text-2xl font-bold">{amount}</span>
                     </div>
-                    <Link
-                        href={{
-                            pathname: '/product/[slug]',
-                            query: { slug },
-                        }}
-                        passHref
-                    >
+                    <Link href={linkOptions} passHref>
                         <button className="btn btn-primary btn-sm rounded-md shadow-md w-full">View Product</button>
                     </Link>
                 </div>
