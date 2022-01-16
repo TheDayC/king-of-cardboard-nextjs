c6c298c54c3bc0cbdf012125069c56e0601fefa8
TheDayC <daveconnolly90@gmail.com>
2022-01-16 14:44:24 +0000

    lint fixes

--- components/Shop/index.tsx
+++ components/Shop/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { removeAllCategories, removeAllProductTypes, setUrlProductType } from '../../store/slices/filters';
 import Grid from './Grid';
