64bb1dac1448f4cd764d091968325841e27e9723
TheDayC <daveconnolly90@gmail.com>
2022-01-18 20:20:44 +0000

    Updated page wrapper to 4 padding and updated margins on home page

--- components/Hero/withImage.tsx
+++ components/Hero/withImage.tsx
@@ -24,7 +24,7 @@ export const HeroWithImage: React.FC<HeroWithImageProps> = ({
             <div className="flex flex-col hero-content p-0 lg:flex-row">
                 {shouldReverse ? (
                     <React.Fragment>
-                        <div className="rounded-md shadow-2xl relative overflow-hidden w-full h-40 hidden lg:inline-block lg:w-1/4 lg:h-80">
+                        <div className="rounded-md shadow-2xl relative overflow-hidden w-full h-40 lg:inline-block lg:w-1/4 lg:h-80">
                             <Image
                                 src={image_url || ''}
                                 alt={`${title}-hero-image`}
@@ -68,7 +68,7 @@ export const HeroWithImage: React.FC<HeroWithImageProps> = ({
                                 </Link>
                             )}
                         </div>
-                        <div className="rounded-md shadow-2xl relative overflow-hidden w-full h-40 hidden lg:inline-block lg:w-1/4 lg:h-80">
+                        <div className="rounded-md shadow-2xl relative overflow-hidden w-full h-40 lg:inline-block lg:w-1/4 lg:h-80">
                             <Image
                                 src={image_url || ''}
                                 alt={`${title}-hero-image`}
