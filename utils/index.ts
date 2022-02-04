812ccddd0ff678ca73890d5f644a5ff06a157485
TheDayC <daveconnolly90@gmail.com>
2022-02-04 15:12:10 +0000

    Pulser, icon and confirmation window added to complete order page. Button moved up and made much larger

--- utils/index.ts
+++ utils/index.ts
@@ -9,3 +9,37 @@ export function toTitleCase(str: string): string {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
 }
+
+import { useEffect } from 'react';
+import Router from 'next/router';
+
+export function useWarnIfUnsavedChanges(unsavedChanges: boolean, message: string): void {
+    useEffect(() => {
+        const routeChangeStart = (url: string) => {
+            if (Router.asPath !== url && unsavedChanges && !confirm(message)) {
+                Router.events.emit('routeChangeError');
+                Router.replace(Router, Router.asPath);
+                throw 'Abort route change. Please ignore this error.';
+            }
+        };
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const beforeunload = (e) => {
+            if (unsavedChanges) {
+                e.preventDefault();
+                e.returnValue = message;
+
+                return message;
+            }
+        };
+
+        window.addEventListener('beforeunload', beforeunload);
+        Router.events.on('routeChangeStart', routeChangeStart);
+
+        return () => {
+            window.removeEventListener('beforeunload', beforeunload);
+            Router.events.off('routeChangeStart', routeChangeStart);
+        };
+    }, [unsavedChanges, message]);
+}
