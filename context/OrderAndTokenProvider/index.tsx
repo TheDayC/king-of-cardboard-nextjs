b185e449ec7effbee30b44fb0e412a024b8d680e
TheDayC <daveconnolly90@gmail.com>
2022-02-05 15:01:12 +0000

    Loading spinner added if the token requires resetting

--- context/OrderAndTokenProvider/index.tsx
+++ context/OrderAndTokenProvider/index.tsx
@@ -7,6 +7,7 @@ import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect';
 import selector from './selector';
 import { fetchToken } from '../../store/slices/global';
 import { createCLOrder } from '../../store/slices/cart';
+import Loading from '../../components/Loading';
 
 const OrderAndTokenProvider: React.FC = ({ children }) => {
     const { accessToken, expires, shouldCreateOrder } = useSelector(selector);
@@ -16,15 +17,15 @@ const OrderAndTokenProvider: React.FC = ({ children }) => {
     const hasExpired = Boolean(expires && expiryDate < currentDate);
     const session = useSession();
     const isGuest = !Boolean(session && session.status === 'authenticated');
-    const shouldResetToken = localStorage.getItem('kingofcardboard-401');
+    const shouldResetToken = !accessToken || hasExpired || localStorage.getItem('kingofcardboard-401') === 'true';
 
     // If accessToken doesn't exist create one.
     useIsomorphicLayoutEffect(() => {
-        if (!accessToken || hasExpired || shouldResetToken === 'true') {
+        if (shouldResetToken) {
             dispatch(fetchToken());
             localStorage.setItem('kingofcardboard-401', 'false');
         }
-    }, [accessToken, hasExpired, shouldResetToken]);
+    }, [shouldResetToken]);
 
     // If the order doesn't exist then create one.
     useIsomorphicLayoutEffect(() => {
@@ -32,6 +33,11 @@ const OrderAndTokenProvider: React.FC = ({ children }) => {
             dispatch(createCLOrder({ accessToken, isGuest }));
         }
     }, [shouldCreateOrder, accessToken, isGuest]);
+
+    // If we need to reset the token then show a loading screen
+    if (shouldResetToken) {
+        return <Loading show={true} />;
+    }
 
     return <React.Fragment>{children}</React.Fragment>;
 };
