491b457d756da4ee3a710c9b39e94fa77ed4c838
TheDayC <daveconnolly90@gmail.com>
2022-02-09 20:17:35 +0000

    Removed RandomSlot component and just integrated the functionality into PickSlot

--- components/Break/Slots/RandomSlot/selector.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-
-import { selectGlobalData, selectBreaksData } from '../../../../store/state/selectors';
-
-const selector = createSelector([selectGlobalData, selectBreaksData], (global, breaks) => ({
-    breakSlots: breaks.currentBreak.breakSlots,
-}));
-
-export default selector;
