37262f26aafbc181ba0300ea982b23bc46e18db1
TheDayC <daveconnolly90@gmail.com>
2022-02-21 16:52:25 +0000

    Added Cors middleware and a handler for approveOrder webhook

--- pages/api/webhook/approveOrder.ts
+++ pages/api/webhook/approveOrder.ts
@@ -1,9 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import Cors from 'cors';
 
 import { parseAsNumber, safelyParse } from '../../../utils/parsers';
 import { apiErrorHandler } from '../../../middleware/errors';
+import { runMiddleware } from '../../../middleware/api';
+
+// Initializing the cors middleware
+const cors = Cors({
+    origin: ['/.kingofcardboard.co.uk$/', '/.king-of-cardboard.commercelayer.io$/'],
+    methods: ['POST', 'HEAD'],
+});
 
 async function approveOrder(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+    // Run the middleware
+    await runMiddleware(req, res, cors);
+
     if (req.method === 'POST') {
         try {
             console.log(req.body);
