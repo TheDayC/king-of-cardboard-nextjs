bf7983521630c11c9638c0abb49f0963078aad64
Dave Connolly <daveconnolly90@gmail.com>
2022-04-23 09:37:15 +0100

    Added a card to point people at the instagram page for breaks

---
+++ components/Breaks/InstagramMessage/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { BsInstagram } from 'react-icons/bs';
+
+const cardImg =
+    'https://images.ctfassets.net/qeycwswfx7l5/1vySGCpOHOG5RvZYyKbah1/4ef5dc9a3be74417b28070aee6979c7d/break-7-front.png?w=400&q=100';
+
+export const InstagramMessage: React.FC = () => {
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <div className="card lg:card-side bg-base-100 shadow-xl">
+                <figure>
+                    <img src={cardImg} alt="Break image" title="Break image" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Our breaks are now on Instagram!</h2>
+                    <p className="mb-4">
+                        As we develop our reputation and continue to offer the best breaking experience in the UK, we
+                        have temporarily migrated our break offerings to our Instagram page.
+                    </p>
+                    <p className="mb-4">
+                        Our goals are still the same but our focus has shifted slightly. You can still find sealed,
+                        singles and everything in between in our shop, that won't go away, but for now breaks will
+                        happen in the Instagram ecosystem.
+                    </p>
+                    <p className="mb-4">
+                        If you wish to get involved with our breaks please click the button below and head over to our
+                        Instagram page that will allow you to claim slots by commenting, or even negotiate deals with
+                        us!
+                    </p>
+                    <div className="card-actions justify-end">
+                        <a
+                            href="https://instagram.com/kocardboard"
+                            target="__blank"
+                            role="link"
+                            className="btn btn-primary"
+                        >
+                            <BsInstagram className="text-2xl inline-block mr-2 hover:text-primary" />
+                            Instagram
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default InstagramMessage;
--- components/Breaks/index.tsx
+++ components/Breaks/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BreakTabs } from '../../enums/breaks';
 
 import Grid from './Grid';
+import InstagramMessage from './InstagramMessage';
+
+const baseClassName = 'tab tab-bordered w-1/2';
 
 export const Breaks: React.FC = () => {
+    const [currentTab, setCurrentTab] = useState(BreakTabs.Live);
+    const isLiveTab = currentTab === BreakTabs.Live;
+    const isArchiveTab = currentTab === BreakTabs.Archive;
+    const liveClassName = `${baseClassName}${isLiveTab ? ' tab-active' : ''}`;
+    const archiveClassName = `${baseClassName}${isArchiveTab ? ' tab-active' : ''}`;
+
+    const handleLiveTabClick = () => {
+        setCurrentTab(BreakTabs.Live);
+    };
+
+    const handleArchiveTabClick = () => {
+        setCurrentTab(BreakTabs.Archive);
+    };
+
     return (
         <div className="block relative w-full">
-            <Grid />
+            <div className="tabs flex-row w-full justify-start items-center mb-6">
+                <a className={liveClassName} onClick={handleLiveTabClick}>
+                    Live Breaks
+                </a>
+                <a className={archiveClassName} onClick={handleArchiveTabClick}>
+                    Archive
+                </a>
+            </div>
+            {isLiveTab && <InstagramMessage />}
+            {isArchiveTab && <Grid />}
         </div>
     );
 };
