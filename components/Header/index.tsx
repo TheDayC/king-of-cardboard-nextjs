5d68b0411eb1e75cd663fcab22ff0b063f15a6c4
Dave Connolly <daveconnolly90@gmail.com>
2022-09-16 18:14:20 +0100

    Removed issue banner

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -173,7 +173,7 @@ export const Header: React.FC = () => {
                 </div>
             </div>
             <NewsBanner />
-            <IssueBanner />
+            {/* <IssueBanner /> */}
         </React.Fragment>
     );
 };
