16a906f8558be736feb0acf62dae62204f05a532
Dave Connolly <daveconnolly90@gmail.com>
2022-09-19 14:49:14 +0100

    Added content to shop and sub pages

--- pages/shop/[type].tsx
+++ pages/shop/[type].tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { GetServerSideProps } from 'next';
 import { startCase, upperCase } from 'lodash';
 import { useDispatch } from 'react-redux';
+import { Document } from '@contentful/rich-text-types';
 
 import PageWrapper from '../../components/PageWrapper';
 import { parseAsString, safelyParse } from '../../utils/parsers';
@@ -11,16 +12,20 @@ import { removeAllProductTypes, setUrlProductType } from '../../store/slices/fil
 import { createToken } from '../../utils/auth';
 import { CreateToken } from '../../types/commerce';
 import { setAccessToken, setExpires } from '../../store/slices/global';
+import { pageBySlug } from '../../utils/pages';
+import Content from '../../components/Content';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const shopType = safelyParse(context, 'query.type', parseAsString, null);
     const accessToken = await createToken();
+    const content = await pageBySlug(shopType, 'shop/');
 
     return {
         props: {
             errorCode: !shopType ? 404 : null,
             shopType,
             accessToken,
+            content,
         },
     };
 };
@@ -28,9 +33,10 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 interface ShopTypeProps {
     shopType: string | null;
     accessToken: CreateToken;
+    content: Document[] | null;
 }
 
-export const ShopType: React.FC<ShopTypeProps> = ({ shopType, accessToken }) => {
+export const ShopType: React.FC<ShopTypeProps> = ({ shopType, accessToken, content }) => {
     const dispatch = useDispatch();
     const shouldUpperCase = shopType === 'wwe' || shopType === 'ufc';
     const caseChangedShopType = shouldUpperCase ? upperCase(shopType || '') : startCase(shopType || '');
@@ -53,9 +59,12 @@ export const ShopType: React.FC<ShopTypeProps> = ({ shopType, accessToken }) => 
             title={`${caseChangedShopType} | Shop | King of Cardboard`}
             description={`${caseChangedShopType} cards, sealed product and memorabilia.`}
         >
-            <div className="flex flex-col w-full relative md:flex-row">
-                <Filters />
-                <Grid />
+            <div className="flex flex-col w-full relative">
+                {content && <Content content={content} />}
+                <div className="flex flex-col w-full relative md:flex-row">
+                    <Filters />
+                    <Grid />
+                </div>
             </div>
         </PageWrapper>
     );
--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { GetServerSideProps } from 'next';
 import { useDispatch } from 'react-redux';
+import { Document } from '@contentful/rich-text-types';
 
 import PageWrapper from '../../components/PageWrapper';
 import Filters from '../../components/Shop/Filters';
@@ -9,23 +10,28 @@ import { removeAllCategories, removeAllProductTypes } from '../../store/slices/f
 import { setAccessToken, setExpires } from '../../store/slices/global';
 import { CreateToken } from '../../types/commerce';
 import { createToken } from '../../utils/auth';
+import { pageBySlug } from '../../utils/pages';
+import Content from '../../components/Content';
 
 export const getServerSideProps: GetServerSideProps = async () => {
     const accessToken = await createToken();
+    const content = await pageBySlug('shop', '');
 
     return {
         props: {
             errorCode: !accessToken ? 500 : null,
             accessToken,
+            content,
         },
     };
 };
 
 interface ShopProps {
     accessToken: CreateToken;
+    content: Document[] | null;
 }
 
-export const ShopPage: React.FC<ShopProps> = ({ accessToken }) => {
+export const ShopPage: React.FC<ShopProps> = ({ accessToken, content }) => {
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -41,11 +47,14 @@ export const ShopPage: React.FC<ShopProps> = ({ accessToken }) => {
     return (
         <PageWrapper
             title="Shop - King of Cardboard"
-            description="A broad selection of sports and trading card singles and sealed products."
+            description="A broad selection of sports cards sealed, single and pack products."
         >
-            <div className="flex flex-col w-full relative md:flex-row">
-                <Filters />
-                <Grid />
+            <div className="flex flex-col w-full relative">
+                {content && <Content content={content} />}
+                <div className="flex flex-col w-full relative md:flex-row">
+                    <Filters />
+                    <Grid />
+                </div>
             </div>
         </PageWrapper>
     );
