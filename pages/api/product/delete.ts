419f9120daaba5db3ab6c7ce59cc830bbad28e97
Dave Connolly <daveconnolly90@gmail.com>
2022-11-26 19:25:59 +0000

    Added the delete product endpoint

---
+++ pages/api/product/delete.ts
@@ -0,0 +1,44 @@
+import { DateTime } from 'luxon';
+import { ObjectId } from 'mongodb';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { connectToDatabase } from '../../../middleware/database';
+import { errorHandler } from '../../../middleware/errors';
+import {
+    parseAsArrayOfStrings,
+    parseAsBoolean,
+    parseAsNumber,
+    parseAsString,
+    safelyParse,
+} from '../../../utils/parsers';
+
+const defaultErr = 'Product could not be updated.';
+
+async function deleteProduct(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+    if (req.method === 'DELETE') {
+        try {
+            const { db } = await connectToDatabase();
+
+            const productsCollection = db.collection('products');
+            const sku = safelyParse(req, 'body.sku', parseAsString, null);
+            const existingProduct = await productsCollection.findOne({ sku });
+
+            if (!existingProduct) {
+                res.status(400).json({ message: 'Product does not exist.' });
+                return;
+            }
+
+            await productsCollection.deleteOne({ sku });
+
+            res.status(201).end();
+        } catch (err: unknown) {
+            const status = safelyParse(err, 'response.status', parseAsNumber, 500);
+
+            res.status(status).json(errorHandler(err, defaultErr));
+        }
+
+        return Promise.resolve();
+    }
+}
+
+export default deleteProduct;
