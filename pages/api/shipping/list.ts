fd97a09ffc632e3b12383196a367669b227d2aca
Dave Connolly <daveconnolly90@gmail.com>
2022-11-27 08:28:46 +0000

    Added list shipping methods endpoint

---
+++ pages/api/shipping/list.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { connectToDatabase } from '../../../middleware/database';
+import { errorHandler } from '../../../middleware/errors';
+import { parseAsNumber, safelyParse } from '../../../utils/parsers';
+
+const defaultErr = 'Could not find any shipping methods.';
+
+async function listShipping(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+    if (req.method === 'GET') {
+        try {
+            const { db } = await connectToDatabase();
+
+            const shippingCollection = db.collection('shipping');
+            const count = safelyParse(req, 'body.count', parseAsNumber, 10);
+            const page = safelyParse(req, 'body.page', parseAsNumber, 0);
+
+            const shippingCount = await shippingCollection.countDocuments();
+            const shippingList = await shippingCollection.find().skip(page).limit(count).toArray();
+
+            if (shippingList.length === 0) {
+                res.status(400).json({ message: defaultErr });
+                return;
+            }
+
+            res.status(200).json({ shippingMethods: shippingList, count: shippingCount });
+        } catch (err: unknown) {
+            const status = safelyParse(err, 'response.status', parseAsNumber, 500);
+
+            res.status(status).json(errorHandler(err, defaultErr));
+        }
+
+        return Promise.resolve();
+    }
+}
+
+export default listShipping;
