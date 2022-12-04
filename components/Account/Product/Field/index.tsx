0fd0a4c2dc4e997cf30da0bc545d5a836780ed82
Dave Connolly <daveconnolly90@gmail.com>
2022-12-04 21:02:15 +0000

    Added title, slug and sku fields to the product add page

---
+++ components/Account/Product/Field/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FieldValues, UseFormRegister } from 'react-hook-form';
+import { IconType } from 'react-icons/lib';
+
+interface ProductFieldProps {
+    instruction: string;
+    placeholder: string;
+    fieldName: string;
+    error: string | null;
+    register: UseFormRegister<FieldValues>;
+    Icon: IconType;
+}
+
+export const ProductField: React.FC<ProductFieldProps> = ({
+    instruction,
+    placeholder,
+    fieldName,
+    error,
+    register,
+    Icon,
+}) => {
+    return (
+        <div className="form-control inline-block">
+            <label className="input-group input-group-md">
+                <span className="bg-base-200">
+                    <Icon className="w-5 h-5" />
+                </span>
+                <input
+                    type="text"
+                    placeholder={placeholder}
+                    {...register(fieldName, {
+                        required: { value: true, message: instruction },
+                    })}
+                    className={`input input-md input-bordered w-full${error ? ' input-error' : ''}`}
+                />
+            </label>
+            {error && (
+                <label className="label">
+                    <span className="label-text-alt">{error}</span>
+                </label>
+            )}
+        </div>
+    );
+};
+
+export default ProductField;
---
+++ components/Account/Product/add.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useSession } from 'next-auth/react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
+import ProductField from './Field';
+import { parseAsString, safelyParse } from '../../../utils/parsers';
+import { BsFillCartCheckFill } from 'react-icons/bs';
+import { MdOutlineTitle, MdOutlineHttp } from 'react-icons/md';
+import { ImFontSize } from 'react-icons/im';
+import { AiOutlineBarcode } from 'react-icons/ai';
+
+export const AddProduct: React.FC = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+        setValue,
+    } = useForm();
+    const hasErrors = Object.keys(errors).length > 0;
+    const titleErr = safelyParse(errors, 'title.message', parseAsString, null);
+    const slugErr = safelyParse(errors, 'slug.message', parseAsString, null);
+    const skuErr = safelyParse(errors, 'sku.message', parseAsString, null);
+
+    const onSubmit: SubmitHandler<FieldValues> = async (data: FieldValues) => {
+        if (hasErrors) {
+            return;
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col">
+                <div className="flex flex-row space-x-4 items-start justify-start">
+                    <ProductField
+                        placeholder="Title"
+                        fieldName="title"
+                        instruction="Title is required."
+                        error={titleErr}
+                        register={register}
+                        Icon={MdOutlineTitle}
+                    />
+                    <ProductField
+                        placeholder="Slug"
+                        fieldName="slug"
+                        instruction="Slug is required."
+                        error={slugErr}
+                        register={register}
+                        Icon={ImFontSize}
+                    />
+                    <ProductField
+                        placeholder="SKU"
+                        fieldName="sku"
+                        instruction="SKU is required."
+                        error={skuErr}
+                        register={register}
+                        Icon={AiOutlineBarcode}
+                    />
+                </div>
+
+                <div className="form-control mt-6">
+                    <button type="submit" className="btn btn-block btn-primary rounded-md">
+                        <BsFillCartCheckFill className="inline-block text-xl mr-2" />
+                        Add product
+                    </button>
+                </div>
+            </div>
+        </form>
+    );
+};
+
+export default AddProduct;
