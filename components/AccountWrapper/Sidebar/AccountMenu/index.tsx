fd3e3004e8dac0ef11d564596bb1f0dedd9baf20
Dave Connolly <daveconnolly90@gmail.com>
2022-12-04 16:22:16 +0000

    Added the admin menu, protected it and added a link to products

---
+++ components/AccountWrapper/Sidebar/AccountMenu/index.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import Link from 'next/link';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux';
+import { MdOutlineAccountCircle } from 'react-icons/md';
+
+import { parseAsString, safelyParse } from '../../../../utils/parsers';
+import { Slugs } from '../../../../enums/account';
+import { setUserId, setUserToken } from '../../../../store/slices/global';
+import Title from '../Title';
+
+export const AccountMenu: React.FC = () => {
+    const dispatch = useDispatch();
+    const router = useRouter();
+    const slug = safelyParse(router, 'query.slug', parseAsString, null);
+
+    const handleLogout = () => {
+        dispatch(setUserToken(null));
+        dispatch(setUserId(null));
+        signOut();
+    };
+
+    return (
+        <div className="flex flex-col">
+            <Title title="Account" Icon={MdOutlineAccountCircle} />
+            <ul className="menu menu-vertical">
+                <li
+                    className={`${
+                        slug === Slugs.Profile ? 'bordered' : 'hover-bordered'
+                    } text-white hover:bg-neutral-focus`}
+                    role="menuitem"
+                >
+                    <Link
+                        href={{
+                            pathname: '/account/[slug]',
+                            query: { slug: Slugs.Profile },
+                        }}
+                    >
+                        Profile
+                    </Link>
+                </li>
+                <li
+                    className={`${
+                        slug === Slugs.AddressBook ? 'bordered' : 'hover-bordered'
+                    } text-white hover:bg-neutral-focus`}
+                    role="menuitem"
+                >
+                    <Link
+                        href={{
+                            pathname: '/account/[slug]',
+                            query: { slug: Slugs.AddressBook },
+                        }}
+                    >
+                        Address Book
+                    </Link>
+                </li>
+                <li
+                    className={`${
+                        slug === Slugs.OrderHistory ? 'bordered' : 'hover-bordered'
+                    } text-white hover:bg-neutral-focus`}
+                    role="menuitem"
+                >
+                    <Link
+                        href={{
+                            pathname: '/account/[slug]',
+                            query: { slug: Slugs.OrderHistory },
+                        }}
+                    >
+                        Order History
+                    </Link>
+                </li>
+                <li
+                    className={`${
+                        slug === Slugs.Achievements ? 'bordered' : 'hover-bordered'
+                    } text-white hover:bg-neutral-focus`}
+                    role="menuitem"
+                >
+                    <Link
+                        href={{
+                            pathname: '/account/[slug]',
+                            query: { slug: Slugs.Achievements },
+                        }}
+                    >
+                        Achievements
+                    </Link>
+                </li>
+                <li
+                    className={`${
+                        slug === Slugs.Achievements ? 'bordered' : 'hover-bordered'
+                    } text-white hover:bg-neutral-focus`}
+                    role="menuitem"
+                >
+                    <a onClick={handleLogout} data-testid="logout">
+                        Log Out
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default AccountMenu;
--- components/AccountWrapper/Sidebar/AdminMenu/index.tsx
+++ components/AccountWrapper/Sidebar/AdminMenu/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { MdOutlineAdminPanelSettings } from 'react-icons/md';
 
 import { parseAsRole, safelyParse } from '../../../../utils/parsers';
 import { Roles } from '../../../../enums/auth';
-import Title from './title';
+import Title from '../Title';
 
 export const AdminMenu: React.FC = () => {
     const { data: session } = useSession();
@@ -15,7 +16,7 @@ export const AdminMenu: React.FC = () => {
 
     return (
         <div className="flex flex-col">
-            <Title title="Admin" />
+            <Title title="Admin" Icon={MdOutlineAdminPanelSettings} />
             <ul className="menu menu-vertical">
                 <li className="hover-bordered text-white">
                     <Link href="/account/products" passHref className="p-3 bg-neutral hover:bg-neutral-focus">
