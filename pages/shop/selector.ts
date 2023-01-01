b8e5ff34f562a3dd6672f334c4c3deb845dff458
Dave Connolly <daveconnolly90@gmail.com>
2023-01-01 20:35:19 +0000

    Fixed show rows criteria

--- pages/shop/selector.ts
+++ pages/shop/selector.ts
@@ -1,14 +1,16 @@
 import { createSelector } from '@reduxjs/toolkit';
+import { isEqual } from 'lodash';
 
 import { SortOption } from '../../enums/products';
 import { selectFiltersData } from '../../store/state/selectors';
+import { DEFAULT_STOCK_STATUSES } from '../../utils/constants';
 
 const selector = createSelector([selectFiltersData], (filters) => ({
     shouldShowRows:
         filters.categories.length === 0 &&
         filters.configurations.length === 0 &&
         filters.interests.length === 0 &&
-        filters.stockStatus.length === 0,
+        isEqual(filters.stockStatus, DEFAULT_STOCK_STATUSES),
     searchTerm: filters.searchTerm,
     sortOption: filters.sortOption,
     hasSearchTerm: filters.searchTerm.length > 0,
