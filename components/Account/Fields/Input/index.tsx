f2da426c24a119cac177a5c6d8b753ba79c49d5b
Dave Connolly <daveconnolly90@gmail.com>
2023-01-02 12:51:03 +0000

    Added a release date and calendar

--- components/Account/Fields/Input/index.tsx
+++ components/Account/Fields/Input/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { UseFormRegister } from 'react-hook-form';
 import { IconType } from 'react-icons/lib';
+import DatePicker from 'react-datepicker';
 
 interface InputFieldProps {
     instruction: string;
@@ -12,6 +13,9 @@ interface InputFieldProps {
     Icon: IconType;
     isRequired: boolean;
     defaultValue?: string | number;
+    isDate?: boolean;
+    startDate?: Date | null;
+    setStartDate?: (date: Date | null) => void;
 }
 
 export const InputField: React.FC<InputFieldProps> = ({
@@ -24,6 +28,9 @@ export const InputField: React.FC<InputFieldProps> = ({
     Icon,
     isRequired,
     defaultValue,
+    isDate = false,
+    startDate,
+    setStartDate,
 }) => {
     return (
         <div className="form-control inline-block">
@@ -31,15 +38,29 @@ export const InputField: React.FC<InputFieldProps> = ({
                 <span className="bg-base-200">
                     <Icon className="w-5 h-5" />
                 </span>
-                <input
-                    type={fieldType}
-                    placeholder={placeholder}
-                    {...register(fieldName, {
-                        required: { value: isRequired, message: instruction },
-                    })}
-                    className={`input input-md input-bordered w-full${error ? ' input-error' : ''}`}
-                    defaultValue={defaultValue}
-                />
+                {isDate && setStartDate ? (
+                    <DatePicker
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                        wrapperClassName="inline-block border-2 border-gray-500 w-auto h-full"
+                        className="input input-md input-bordered w-full"
+                        isClearable
+                        placeholderText={placeholder}
+                        clearButtonClassName="btn btn-square rounded-l-none after:bg-transparent after:content-['×'] after:text-xl after:leading-after-content"
+                        dateFormat="dd/MM/yyyy"
+                        showYearDropdown
+                    />
+                ) : (
+                    <input
+                        type={fieldType}
+                        placeholder={placeholder}
+                        {...register(fieldName, {
+                            required: { value: isRequired, message: instruction },
+                        })}
+                        className={`input input-md input-bordered w-full${error ? ' input-error' : ''}`}
+                        defaultValue={defaultValue}
+                    />
+                )}
             </label>
             {error && (
                 <label className="label">
