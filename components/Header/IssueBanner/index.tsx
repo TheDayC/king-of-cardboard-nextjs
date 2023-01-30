5b681c543b1a18f6e8665bc2db10756b3747c565
Dave Connolly <daveconnolly90@gmail.com>
2023-01-30 22:26:52 +0000

    Added option fetch to issue banner

--- components/Header/IssueBanner/index.tsx
+++ components/Header/IssueBanner/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { getOptions } from '../../../utils/account/options';
 
 export const IssueBanner: React.FC = () => {
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const fetchOptions = async () => {
+        const { errorMessage } = await getOptions();
+
+        setErrorMessage(errorMessage);
+    };
+
+    useEffect(() => {
+        fetchOptions();
+    }, []);
+
+    if (errorMessage.length === 0) {
+        return null;
+    }
+
     return (
         <div className="flex flex-row justify-center w-full p-2 bg-red-600 text-neutral-content shadow-md text-sm md:text-md">
-            <p>
-                King of Cardboard will closed from 2nd - 13th Feb. Any orders placed after 30th Jan are not guaranteed
-                to be fulfilled prior to closure.
-            </p>
+            <p>{errorMessage}</p>
         </div>
     );
 };
