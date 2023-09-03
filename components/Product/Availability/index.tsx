e4f95c32d459dde00b3c02be654c090acdd54b3b
Dave Connolly <daveconnolly90@gmail.com>
2023-09-03 15:13:37 +0100

    Added availability component

---
+++ components/Product/Availability/index.tsx
@@ -0,0 +1,35 @@
+import { DateTime } from 'luxon';
+import React from 'react';
+import { BsBoxSeam, BsCalendarDate, BsCart } from 'react-icons/bs';
+import { FaPlaneArrival } from 'react-icons/fa';
+
+import { StockStatus } from '../../../enums/products';
+import { getStockStatusColor } from '../../../utils/account/products';
+
+interface AvailabilityProps {
+    isAvailable: boolean;
+    stockStatus: StockStatus;
+}
+
+export const Availability: React.FC<AvailabilityProps> = ({ isAvailable, stockStatus }) => {
+    const isImport = stockStatus === StockStatus.Import;
+    const isPreOrder = stockStatus === StockStatus.PreOrder;
+
+    const stockStatusColor = getStockStatusColor(stockStatus);
+
+    if (isPreOrder) {
+        return <span style={{ color: stockStatusColor }}>Pre-Order</span>;
+    }
+
+    if (isImport) {
+        return <span style={{ color: stockStatusColor }}>Import</span>;
+    }
+
+    if (isAvailable) {
+        return <span className="text-green-600">In Stock</span>;
+    }
+
+    return <span className="text-red-600">Out of Stock</span>;
+};
+
+export default Availability;
