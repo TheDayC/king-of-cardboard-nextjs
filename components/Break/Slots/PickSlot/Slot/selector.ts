ffcc4e63253e8b79c04f306d59ed96cf6116d219
Dave Connolly <daveconnolly90@gmail.com>
2023-09-24 17:49:02 +0100

    Removed old Break JS

--- components/Break/Slots/PickSlot/Slot/selector.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-
-import { selectCartData } from '../../../../../store/state/selectors';
-
-const selector = createSelector([selectCartData], (cart) => ({
-    items: cart.items,
-}));
-
-export default selector;
--- components/Break/Slots/selector.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-
-import { selectBreaksData } from '../../../store/state/selectors';
-
-const selector = createSelector([selectBreaksData], (breaks) => ({
-    format: breaks.currentBreak.format,
-}));
-
-export default selector;
